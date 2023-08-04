6888ad1b0ce4b26c50619ca859978d6c306cd42e
morganbarrett <morganbarrett@icloud.com>
2023-08-04 14:54:02 +0100

    feat: added code from farm

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,9 +1,9 @@
-import { FunctionComponent } from "react";
-import { TextProps } from "react-native";
+import {FunctionComponent} from "react";
+import {TextProps} from "react-native";
 
 export interface LabelProps extends TextProps {
-  type?: string; //todo replace with literals
-  bold?: boolean;
+	type?: string; //todo replace with literals
+	bold?: boolean;
 }
 
 export declare const Label: FunctionComponent<LabelProps>;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,66 @@
-export {Text as Label} from "react-native";
+import {Text} from "react-native";
+import {} from "@react-native-ui-components/color";
+
+//todo might end up using native components
+
+const types = {
+	body: {
+		fontSize: 17,
+		lineHeight: 22
+	},
+	callout: {
+		fontSize: 16,
+		lineHeight: 21
+	},
+	caption1: {
+		fontSize: 12,
+		lineHeight: 16
+	},
+	caption2: {
+		fontSize: 11,
+		lineHeight: 13
+	},
+	footnote: {
+		fontSize: 13,
+		lineHeight: 18
+	},
+	headline: {
+		fontSize: 17,
+		lineHeight: 22
+	},
+	largeTitle: {
+		fontSize: 34,
+		lineHeight: 41
+	},
+	subheadline: {
+		fontSize: 15,
+		lineHeight: 20
+	},
+	title1: {
+		fontSize: 28,
+		lineHeight: 34
+	},
+	title2: {
+		fontSize: 22,
+		lineHeight: 28
+	},
+	title3: {
+		fontSize: 20,
+		lineHeight: 25
+	}
+};
+
+export const Label = props => (
+	<Text
+		{...props}
+		style={[
+			{
+				color: "black" //todo use color module (textLabel)
+			},
+			types[props.type || "body"],
+			props.style
+		]}
+	>
+		{props.children}
+	</Text>
+);
